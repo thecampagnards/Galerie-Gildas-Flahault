719442ea2dde6c2141881b40b08015b6067d869c
TheCampagnards <konstantin.sidorenko@orange.fr>
2017-03-24 09:44:32 +0100

    [SCSS] - ajout du scss

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,40 @@
-var gulp = require('gulp');
-var chug = require('gulp-chug');
-var argv = require('yargs').argv;
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var chug = require('gulp-chug')
+var argv = require('yargs').argv
 
-config = [
-    '--rootPath',
-    argv.rootPath || '../../../../../../../web/assets/',
-    '--nodeModulesPath',
-    argv.nodeModulesPath || '../../../../../../../node_modules/'
-];
+var config = [
+  '--rootPath',
+  argv.rootPath || '../../../../../../../web/assets/',
+  '--nodeModulesPath',
+  argv.nodeModulesPath || '../../../../../../../node_modules/'
+]
 
-gulp.task('admin', function() {
-    gulp.src('vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Gulpfile.js', { read: false })
-        .pipe(chug({ args: config }))
-    ;
-});
+const RESOURCES_PATH = 'app/Resources'
+const COMPILED_PATH = 'web'
 
-gulp.task('shop', function() {
-    gulp.src('vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Gulpfile.js', { read: false })
-        .pipe(chug({ args: config }))
-    ;
-});
+gulp.task('admin', function () {
+  gulp.src('vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Gulpfile.js', { read: false })
+  .pipe(chug({ args: config }))
+})
 
-gulp.task('default', ['admin', 'shop']);
+gulp.task('shop', function () {
+  gulp.src('vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Gulpfile.js', { read: false })
+  .pipe(chug({ args: config }))
+})
+
+gulp.task('scss', function () {
+  return gulp.src(RESOURCES_PATH + '/scss/*.scss')
+  .pipe(sass({sourceComments: 'map', errLogToConsole: true}))
+  .on('error', function (err) {
+    console.log(err.toString())
+    this.emit('end')
+  })
+  .pipe(gulp.dest(COMPILED_PATH + '/css'))
+})
+
+gulp.task('watch', function () {
+  gulp.watch(RESOURCES_PATH + '/scss/*.scss', ['scss'])
+})
+
+gulp.task('default', ['admin', 'shop', 'scss'])
