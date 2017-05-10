2f5767fdc130e82eca7678833b8ca3e0ea063878
TheCampagnards <konstantin.sidorenko@orange.fr>
2017-05-10 11:36:29 +0200

    [FLECHES] - correction js

--- app/themes/GalerieTheme/js/main.js
+++ app/themes/GalerieTheme/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-  if ($('.item.prev').hasClass('disbled')) {
-    $('.changepage.prev').addClass('disbled')
+  if ($('.item.prev').hasClass('disabled')) {
+    $('.changepage.prev').addClass('disabled')
   } else {
     var prevLink = $('.item.prev').attr('href')
     var prevContent = $('.changepage.prev').html()
     $('.changepage.prev').html('<a href="' + prevLink + '">' + prevContent + '</a>')
   }
 
-  if ($('.item.next').hasClass('disbled')) {
-    $('.changepage.next').addClass('disbled')
+  if ($('.item.next').hasClass('disabled')) {
+    $('.changepage.next').addClass('disabled')
   } else {
     var nextLink = $('.item.next').attr('href')
     var nextContent = $('.changepage.next').html()
