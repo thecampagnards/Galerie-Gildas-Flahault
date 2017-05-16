f66adfed821b693ad819f6ca4028c1528a89c93e
TheCampagnards <konstantin.sidorenko@orange.fr>
2017-05-16 11:14:18 +0200

    [FOOTER] - ajout des pages

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,4 +14,24 @@ class DefaultController extends Controller
               ->getRepository('AppBundle:Actualite')->findAll();
         return $this->render('pages/actualite.html.twig', array('actualites' => $actualites));
     }
+
+    public function aProposAction()
+    {
+        return $this->render('pages/a-propos.html.twig');
+    }
+
+    public function charteDeConfidentialiteAction()
+    {
+        return $this->render('pages/charte-de-confidentialite.html.twig');
+    }
+
+    public function conditionsGeneralesDeVenteAction()
+    {
+        return $this->render('pages/conditions-generales-de-vente.html.twig');
+    }
+
+    public function LivraisonExpeditionAction()
+    {
+        return $this->render('pages/livraison-&-expedition.html.twig');
+    }
 }
